4192c5cdffcaf8e3fe35eb572df1fb729b581e60
davinegreiiros <davi.negreiiros36@gmail.com>
2022-06-25 12:24:48 -0300

    Exercicio 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,9 +1,20 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    beforeEach( function (){
         cy.visit('./src/index.html')
-    
+    })
+    it('verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it.only('preenche os campos obrigatórios e envia o formulário', function() {
+        cy.get('#firstName').type('Davi')
+        cy.get('#lastName').type('Negreiros')
+        cy.get('#email').type('davi.negreiiros36@gmail.com')
+        cy.get('#open-text-area').type('Teste ')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
     })
   })
