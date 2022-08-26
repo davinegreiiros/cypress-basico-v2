263c9055305d98a4e9194e85de6ac44acdc5dbd0
DAVI BEZERRA NEGREIROS GOMES <davi.gomes@unimedfortaleza.com.br>
2022-08-26 16:09:10 -0300

    exercicio extra 2

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -20,4 +20,15 @@ describe('Central de Atendimento ao Cliente TAT', function () {
 
         cy.get('.success').should('be.visible')
     })
+
+    it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
+        cy.get('#firstName').type('Davi')
+        cy.get('#lastName').type('Negreiros')
+        cy.get('#email').type('davi.negreiiros36@gmail,com')
+        cy.get('#open-text-area').type('Teste')
+
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
+    })
 })
