10e95a62767008a1df6906600408eb6cbac80a71
DAVI BEZERRA NEGREIROS GOMES <davi.gomes@unimedfortaleza.com.br>
2022-08-26 15:55:27 -0300

    exercicio extra 01

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,20 +1,23 @@
 /// <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function() {
-    beforeEach( function (){
+describe('Central de Atendimento ao Cliente TAT', function () {
+    beforeEach(function () {
         cy.visit('./src/index.html')
     })
-    it('verifica o título da aplicação', function() {
+    it('verifica o título da aplicação', function () {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
-    it.only('preenche os campos obrigatórios e envia o formulário', function() {
+    it.only('preenche os campos obrigatórios e envia o formulário', function () {
+        const longText = 'Teste teste teste testes teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste'
         cy.get('#firstName').type('Davi')
         cy.get('#lastName').type('Negreiros')
         cy.get('#email').type('davi.negreiiros36@gmail.com')
-        cy.get('#open-text-area').type('Teste ')
+        cy.get('#open-text-area').type(longText, {
+            delay: 0
+        })
         cy.get('button[type="submit"]').click()
 
         cy.get('.success').should('be.visible')
     })
-  })
+})
