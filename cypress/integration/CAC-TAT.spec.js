7cd5b6fa6468e1d024ed72781d3ae70fa09f8188
davinegreiiros <davi.negreiiros36@gmail.com>
2022-11-05 00:06:07 -0300

    exercicio 5

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -37,7 +37,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         .type('abcdefghij')
         .should('have.value', '')
     })
-
+    //exercicio 04
     it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function () {
         cy.get('#firstName').type('Davi')
         cy.get('#lastName').type('Negreiros')
@@ -47,5 +47,14 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('button[type="submit"]').click()
 
         cy.get('.error').should('be.visible')
+    })
+
+    //exercicio 05
+    it.only('Preenche e limpa os campos nome, sobrenome, email e telefone ', function () {
+        cy.get('#firstName').type('Davi').should('have.value', 'Davi').clear().should('have.value', '');
+        cy.get('#lastName').type('Negreiros').should('have.value', 'Negreiros').clear().should('have.value', '');
+        cy.get('#email').type('davi.negreiiros36@gmail.com').should('have.value', 'davi.negreiiros36@gmail.com').clear().should('have.value', '');
+        cy.get('#phone').type('123456789').should('have.value', '123456789').clear().should('have.value', '');
+
     })
 })
