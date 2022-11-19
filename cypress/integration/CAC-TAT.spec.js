7d58724c21a3f0ffc3885e8b9dc364d5c5a654a8
davinegreiiros <davi.negreiiros36@gmail.com>
2022-11-19 12:40:41 -0300

    exercicio 6 e 7

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -4,12 +4,13 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     beforeEach(function () {
         cy.visit('./src/index.html')
     })
-    it('verifica o título da aplicação', function () {
+    it('Verifica o título da aplicação', function () {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
-    it.only('preenche os campos obrigatórios e envia o formulário', function () {
+    it.only('Preenche os campos obrigatórios e envia o formulário', function () {
         const longText = 'Teste teste teste testes teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste'
+        
         cy.get('#firstName').type('Davi')
         cy.get('#lastName').type('Negreiros')
         cy.get('#email').type('davi.negreiiros36@gmail.com')
@@ -21,7 +22,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('.success').should('be.visible')
     })
 
-    it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
+    it.only('Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
         cy.get('#firstName').type('Davi')
         cy.get('#lastName').type('Negreiros')
         cy.get('#email').type('davi.negreiiros36@gmail,com')
@@ -38,7 +39,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         .should('have.value', '')
     })
     //exercicio 04
-    it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function () {
+    it.only('Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function () {
         cy.get('#firstName').type('Davi')
         cy.get('#lastName').type('Negreiros')
         cy.get('#email').type('davi.negreiiros36@gmail.com')
@@ -54,7 +55,20 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#firstName').type('Davi').should('have.value', 'Davi').clear().should('have.value', '');
         cy.get('#lastName').type('Negreiros').should('have.value', 'Negreiros').clear().should('have.value', '');
         cy.get('#email').type('davi.negreiiros36@gmail.com').should('have.value', 'davi.negreiiros36@gmail.com').clear().should('have.value', '');
-        cy.get('#phone').type('123456789').should('have.value', '123456789').clear().should('have.value', '');
-
+        cy.get('#phone').type('1234567890').should('have.value', '1234567890').clear().should('have.value', '');
     })
+
+    //exercicio 06
+    it.only('Exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function () {
+        cy.get('button[type="submit"]').click()
+        cy.get('.error').should('be.visible')
+    })
+
+    //exercicio 07
+    it.only('Envia o formuário com sucesso usando um comando customizado', function() {
+        cy.fillMandatoryFieldsAndSubmit()
+
+        cy.get('.success').should('be.visible')
+    })
+
 })
